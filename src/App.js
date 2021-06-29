c6eb35759d275e7db8c9eb9107637a125c4f611e
alanfl <alanfl81199@gmail.com>
2021-06-29 14:08:07 -0400

    Add event handling and input logic for Bottles.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,69 @@
 import React from 'react';
+import {cloneDeep} from 'lodash'
 import './App.css';
 
-function App() {
+let initialState = {
+  bottles: [50],
+  currentBottle: 0
+}
+
+const Context = React.createContext(initialState)
+
+function Reducer(state, action) {
+  let new_state = cloneDeep(state)
+
+  new_state.bottles[action.payload.idx] = action.payload.val
+
+  return new_state
+}
+
+function App(prop) {
+  let [state, dispatch] = React.useReducer(Reducer, initialState)
+
   return (
-    <div className="App">
-      <Bottle>
-      </Bottle>
-    </div>
+    <Context.Provider value={[state, dispatch]}>
+      <div className="App">
+        <BottleContainer />
+        {
+          state.bottles.map((val, idx) => 
+            <Bottle key={idx} idx={idx} />
+          )
+        }
+      </div>
+    </Context.Provider>
   );
 }
 
-function Bottle(prop) {
-  let currentWaterLevel = 0;
+function BottleContainer(prop) {
+  let [state, dispatch] = React.useContext(Context)
 
   return (
-    <div className='Bottle'>
-      <label
-        for='water-level'
-      >
-        Current Water Level {currentWaterLevel}
-      </label>
+    <div>
+      Current Water Level: {state.bottles[state.currentBottle]}
+    </div>
+  )
+} 
+
+function Bottle(prop) {
+  let [state, dispatch] = React.useContext(Context)
+
+  return (
+    <div className='Bottle'
+      onChange = { (event) =>
+        dispatch({
+          type: 'waterLevelUpdate',
+          payload: {
+            idx: prop.idx,
+            val: event.target.value
+          }
+        })
+      }
+    >
       <input
         type='range'
         id='water-level'
         orient='vertical'
-        min='0'
-        max='100'
+        value={state.bottles[prop.idx]}
       />
       <input
         type='button'
@@ -34,10 +72,6 @@ function Bottle(prop) {
       />
     </div>
   );
-}
-
-function DropdownMenu(prop) {
-
 }
 
 export default App;
